ff4b45255a67bd1a0f16c11d00e7d275ffe35a2b
LluisManas <manastorra@gmail.com>
2020-05-10 11:42:17 +0200

    backend

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const users = require("./models/User");
+const user = require("./routes/api/users");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/projectName", {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("MongoDB connected.."))
+  .catch((err) => console.log(err));
+
+app.use("/api/user", user);
+
+const port = 5000;
+
+app.listen(5000, () => {
+  console.log(`Server started on port ${port}`);
+});
